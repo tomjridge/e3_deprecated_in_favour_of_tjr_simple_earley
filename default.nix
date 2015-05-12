2c3102dfa9e79a4b3159ef964a5dcec0567f9076
Tom Ridge <tom.j.ridge@googlemail.com>
2015-05-12 17:43:02 +0100

    +

---
+++ default.nix
@@ -0,0 +1,47 @@
+# nix-build default.nix -A e3.build
+# nix-shell default.nix -A e3.build
+# nix-shell default.nix -A e3.post_install
+let 
+    pkgs = import <nixpkgs> {};
+    stdenv = pkgs.stdenv;
+    fetchgit = pkgs.fetchgit;
+in rec {
+
+   mke3 = { ocaml, findlib}:
+   rec {
+    build= stdenv.mkDerivation {
+      name = "e3";
+    
+  #    src = fetchgit {
+  #      url = https://github.com/tomjridge/p3.git;
+  #      rev = "0e42a29";
+  #      sha256 = "795b8bacbea102021ad4aaa819d578e58fd7d2041eba60e36482e04e01f81c32";
+  #    };
+      src=./.;
+    
+      buildInputs = [ ocaml findlib ];
+    
+      configurePhase = "true"; 	# Skip configure
+  
+#      installPhase = "true";
+    
+      createFindlibDestdir = true;
+    };
+
+    # get a nix-shell environment with p3 installed via ocamlfind
+    post_install = stdenv.mkDerivation {
+      name = "post_install";
+    
+      buildInputs = [ ocaml findlib build ];
+  
+    };
+
+
+  };
+
+
+  e3 = mke3 { ocaml=pkgs.ocaml_4_02_1; findlib=pkgs.ocamlPackages_4_02_1.findlib; };
+  e3_4_01_0 = mke3 { ocaml=pkgs.ocaml_4_01_0; findlib=pkgs.ocamlPackages_4_01_0.findlib; };
+
+
+}
